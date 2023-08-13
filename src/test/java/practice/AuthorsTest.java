de24ad6ec3064899a26876f8c444aa1c8a0f0ef2
Ahmed Khaled <68038931+ahmedkhaled14@users.noreply.github.com>
2023-08-13 19:14:10 +0300

    🍀RestAssured Demo🍀

--- src/test/java/practice/AuthorsTest.java
+++ src/test/java/practice/AuthorsTest.java
@@ -1,6 +1,7 @@
 package practice;
 
 import POJO.AuthorObj;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,18 +12,34 @@ public class AuthorsTest {
 
     @Test(description = "get last author")
     public void getLastAuthor(){
-        Response response = author.getAllAuthors();
-        response.then().log().body();
 
-        AuthorObj[] data = response.body().as(AuthorObj[].class);
+        /**
+         *                    (Not Required)
+         * get all authors just for comparing with the last Author
+         *                   
+         */
+        Response getAllAuthors = author.getAllAuthors();
+        getAllAuthors.then().log().body();
 
+        /**
+         *  get last author
+         */
+        AuthorObj[] data = getAllAuthors.body().as(AuthorObj[].class);
         String lastAuthorId = String.valueOf(data[data.length-1].getId());
+        Response lastAuthor = author.getLastAuthor(lastAuthorId);
+        lastAuthor.then().log().body();
 
-        System.out.println(" get last author id = " +lastAuthorId);
 
-        author.getLastAuthor(lastAuthorId)
-                .then()
-                .log()
-                .body();
+        /**
+         *                      Validation on ID
+         *
+         * get the last author id from lastAuthor response body and validate that
+         * the Author id
+         * Is equal to
+         * the Expected the Last Author id
+         */
+        JsonPath jsonpath= lastAuthor.jsonPath();
+        String getActualLastAuthorID = String.valueOf(jsonpath.getInt("id"));
+        Assert.assertEquals(getActualLastAuthorID,lastAuthorId);
     }
 }
